6ec60eb09a96f22d5018090dcd3c88533a20b5a4
Adetutu Oluwasanmi <oluwasanmiadetutu@gmail.com>
2023-05-18 00:20:55 +0100

    update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,18 @@ import './App.css';
 
 const App = () => {
 
-  // eslint-disable-next-line no-unused-vars
   const [showEvents, setShowEvents] = useState(true)
-  const [users, updateUsers] = useState([
-    { id: 1, name: "Sophia Anderson", profilePic: "https://t4.ftcdn.net/jpg/02/79/66/93/360_F_279669366_Lk12QalYQKMczLEa4ySjhaLtx1M2u7e6.jpg" },
-    { id: 2, name: "Isabella Sullivan", profilePic: "https://t3.ftcdn.net/jpg/04/67/95/40/360_F_467954064_qFJxeuLVPcXon6X478v9hdgHrRlNgzDB.jpg" },
-    { id: 3, name: "Ava Reynolds", profilePic: "https://us.123rf.com/450wm/moremar/moremar1904/moremar190400023/moremar190400023.jpg?ver=6" },
-    { id: 4, name: "Olivia Thompson", profilePic: "https://img.freepik.com/free-photo/woman-with-long-hair-yellow-shirt-is-standing-front-window_1340-37532.jpg?w=740&t=st=1684334545~exp=1684335145~hmac=b316c4064198323cc324f2e22775b43158b102469d8dc1f10c00cdc22234d642"},
+  const [profiles, updateProfiles] = useState([
+    { id: 1, name: "Sophia Anderson", profilePic: "https://rb.gy/9jh39" },
+    { id: 2, name: "Isabella Sullivan", profilePic: "https://rb.gy/gs5lt" },
+    { id: 3, name: "Ava Reynolds", profilePic: "https://rb.gy/rs32e" },
+    { id: 4, name: "Olivia Thompson", profilePic: "https://rb.gy/3wy0n"},
     
   ])
 
   const handleDelete = (id) => {
-    let remove = users.filter(each => each.id !== id)
-    updateUsers(remove)
+    let filteredProfiles = profiles.filter(each => each.id !== id)
+    updateProfiles(filteredProfiles)
   }
 
 
@@ -28,23 +27,23 @@ return (
 
      <div className='profile-card'>
 
-      {showEvents &&  users.map((user) => (
+      {showEvents &&  profiles.map((profile) => (
 
-          <div key={user.id}>
+          <div key={profile.id}>
 
                 <div className="card">
 
                     <div className="number">
-                        {user.id}
-                    </div>
+                        {profile.id}
+                    </div> 
 
                      <div>
-                        <img className="profile-pic" src={user.profilePic} alt="profile picture" />
+                        <img className="profile-pic" src={profile.profilePic} alt="profile picture" />
                     </div>
 
                     <div className="descr">
-                        <p className="name">{user.name}</p>
-                        <button className="delete-profile" onClick={()=> handleDelete(user.id)}> Delete profile</button>
+                        <p className="name">{profile.name}</p>
+                        <button className="delete-profile" onClick={()=> handleDelete(profile.id)}> Delete profile</button>
                     </div>
 
                 </div>
